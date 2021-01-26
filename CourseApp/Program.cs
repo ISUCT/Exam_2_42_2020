159d5aa8f7041d447893fce2460f4578ed6ac440
Никита Павлов <Nikita.pavlov124@yandex.ru>
2021-01-26 09:56:10 +0300

    fix

--- CourseApp/Program.cs
+++ CourseApp/Program.cs
@@ -8,7 +8,6 @@ namespace CourseApp
     {
         public static void Main()
         {
-            Game game = new Game();
             Console.WriteLine(Game.Play(1, 1));
             Console.WriteLine(Game.Play(1, 2));
             Console.WriteLine(Game.Play(1, 3));
