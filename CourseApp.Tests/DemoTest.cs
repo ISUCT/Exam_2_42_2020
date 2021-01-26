1af848a13ff74dad3c0505f40eac71cdf3bf5c88
Anna2-42 <71084649+Anna2-42@users.noreply.github.com>
2021-01-26 10:41:42 +0300

    exam_Sidorova_Anna

--- CourseApp.Tests/DemoTest.cs
+++ CourseApp.Tests/DemoTest.cs
@@ -12,32 +12,14 @@ namespace CourseApp.Tests
         }
 
         [Theory]
-        [InlineData(0, 0, 0, double.NaN)]
-        [InlineData(2, 4.1, 0.77, 2.671)]
+        [InlineData(0, 0, 0, 1)]
+        [InlineData(3, 2, 0, -5)]
+        [InlineData(3, 2, 1, -2)]
+        [InlineData(3, 2, 5, 10)]
         public void TestCalcAllZeros(double a, double b, double x, double exp)
         {
             var actualResult = Program.Calc(a, b, x);
             Assert.Equal(exp, actualResult, 3);
-        }
-
-        [Fact]
-        public void TestTaskA()
-        {
-            var res = Program.TaskA(2, 4.1, 1, 3, 1);
-            Assert.Equal(3, res.Length);
-            double[] expX = { 1, 2, 3 };
-            for (int i = 0; i <= 2; i++)
-            {
-                var (x, y) = res[i];
-                Assert.Equal(expX[i], x, 1);
-            }
-        }
-
-        [Fact]
-        public void TestTaskAXnGraterXn()
-        {
-            var res = Program.TaskA(2, 4.1, 3, 1, 1);
-            Assert.Empty(res);
         }
     }
 }
