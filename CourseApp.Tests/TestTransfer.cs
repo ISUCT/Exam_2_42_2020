a69fb9594626ed3161c4eb80dec08433dfa88d12
Vladimir_Puchkov <vladimir.puchkov3@mail.ru>
2021-02-09 16:30:43 +0300

    +testi

--- CourseApp.Tests/TestTransfer.cs
+++ CourseApp.Tests/TestTransfer.cs
@@ -18,5 +18,33 @@ namespace CourseApp.Tests
          LengthConverter izmerenie = new LengthConverter();
          Assert.Equal(1, izmerenie.TransferKilometrov(1.609));
         }
+
+        [Fact]
+        public void TestMilOtricatelnogoChisla()
+        {
+         LengthConverter izmerenie = new LengthConverter();
+         Assert.Equal(-1.609, izmerenie.TransferMil(-1));
+        }
+
+        [Fact]
+        public void TestMil0()
+        {
+         LengthConverter izmerenie = new LengthConverter();
+         Assert.Equal(0, izmerenie.TransferMil(0));
+        }
+
+         [Fact]
+        public void TestKilometrovOtricatelnogoChisla()
+        {
+         LengthConverter izmerenie = new LengthConverter();
+         Assert.Equal(-1, izmerenie.TransferKilometrov(-1.609));
+        }
+
+        [Fact]
+        public void TestKilometrov0()
+        {
+         LengthConverter izmerenie = new LengthConverter();
+         Assert.Equal(0, izmerenie.TransferKilometrov(0));
+        }
     }
 }
